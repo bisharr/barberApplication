4d38b9e7be8d5ae88ab99d193cba758c3513958a
Bishar <bishar995@gmail.com>
2025-07-20 08:01:03 +0300

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h2 className="  text-center bg-amber-400 text-white text-3xl p-4 rounded-2xl my-auto">
+        Barber Management system
+      </h2>
+    </>
+  );
+}
+
+export default App;
